619c5da61cbb7bb31ff7348e7b44dcf8b3bdea76
Bernardo Costa Moreira <bernardocostamoreira@Bernardos-MacBook-Pro.local>
2019-03-28 10:41:57 +0000

    gitignore added, I/O files and (-r) might be working

--- Proj1/main.c
+++ Proj1/main.c
@@ -3,55 +3,84 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <dirent.h>
 #define BUFFER_SIZE 256
 
-// int outfile_function(char *name, char *readFile)
-// {
-//     FILE *fp;
-//     FILE *fp2;
-//     fp = fopen(name, "w");
-//     fp2 = fopen(readFile, "r");
+int outfile_function(char *readFile, char *writeFile)
+{
+    FILE *fp1;
+    FILE *fp2;
+    char c;
 
-//     char buffer[BUFFER_SIZE];
-//     ssize_t num_chars;
-//     while ((num_chars = read(fp2, buffer, BUFFER_SIZE)) > 0)
-//     {
-//         write(fp, buffer, num_chars);
-//     }
-//     close(fp);
-//     close(fp2);
+    fp1 = fopen(readFile, "r");
+    if (fp1 == NULL)
+    {
+        printf("Cannot open file :%s\n", readFile);
+        exit(0);
+    }
+    fp2 = fopen(writeFile, "w");
 
-//     return 0;
-// }
+    if (fp2 == NULL)
+    {
+        printf("Cannot open file :%s\n", writeFile);
+        exit(0);
+    }
+
+    c = fgetc(fp1);
+
+    while (c != EOF)
+    {
+        fputc(c, fp2);
+        c = fgetc(fp1);
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
+}
 
 int main(int argc, char **argv, char **env)
 {
-    //outfile_function("f2.txt", "f1.txt");
+    //Read input from one file, and copy to another!
+    outfile_function("f1.txt", "f2.txt");
 
     int option = 0;
-    bool rFlag = false;
-    bool hFlag = false, mda5Flag = false, 1Flag = false, 256Flag = false;
-    bool oFlag = false, outfileFlag = false;
-    bool vFlag = false;
-    bool filedirFlag = false;
+    // bool rFlag = false;
+    DIR *d;
+    struct dirent *dir;
+    // bool hFlag = false, mda5Flag = false, 1Flag = false, 256Flag = false;
+    // bool oFlag = false, outfileFlag = false;
+    // bool vFlag = false;
+    // bool filedirFlag = false;
 
     while ((option = getopt(argc, argv, "rh:m12:o:f:vd")) != -1)
     {
         switch (option)
         {
         case 'r': //rFlag = true;
+
             printf("rFlag ON!!\n");
+            d = opendir(".");
+            if (d)
+            {
+                while ((dir = readdir(d)) != NULL)
+                {
+                    printf("%s\n", dir->d_name);
+                }
+                closedir(d);
+            }
             break;
 
-        case 'h': //hFlag = true;
-            printf("hFlag ON!!\n");
-            break;
+            // case 'h': //hFlag = true;
+            //     printf("hFlag ON!!\n");
+            //     break;
 
-        case 'o':
-            printf("oFlag ON\n");
-            break;
+            // case 'o':
+            //     printf("oFlag ON\n");
+            //     break;
         }
-        printf("%d\n", argc);
+        printf("Input Size:%d\n", argc);
     }
     return 0;
 }
